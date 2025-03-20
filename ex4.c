5be73f81b7c007410f6c5d4537d5f89613ff44f8
D3v4st3d-Kn1ght <enzoayuzuki@gmail.com>
2025-03-19 23:09:35 -0300

    Update ex4.c

--- ex4.c
+++ ex4.c
@@ -2,16 +2,18 @@
 
 int main(void)
 {  
-    int raio = 0;
-    int diametro, circunferencia, area;
-    int PI = 3.1415;
+    float raio;
+    float diametro, circunferencia, area;
+    float PI = 3.1415;
 
     printf("Digite o valor do raio! ");
-    scanf("%d", &);
+    scanf("%f", &raio);
     
     diametro = raio * 2;
     circunferencia = 2 * PI * raio;
     area = PI * (raio * raio);
+    
+    printf("\n Diametro: %.1f Circunferencia: %.1f Area: %.1f", diametro, circunferencia, area);
 
     return 0;
 }
